485be240a943e839848f3f78ca8bba962b60e543
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2024-04-17 10:13:19 +0530

    Initial commit for responsive navbar

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-// import styles from "../styles/header.module.css"
-import "../styles/delete.css"
+import styles from "../styles/header.module.css"
 import { GiHamburgerMenu } from "react-icons/gi";
 const Header = () => {
     const [isOpen, setIsOpen] = useState(false)
@@ -9,37 +8,46 @@ const Header = () => {
         setIsOpen((open) => !open)
     }
     return (
-        <main className='mainContainer'>
-            <nav className="navigationBar res-navigation">
-                <div className="burger" onClick={hambergerMenu}>
-                    <GiHamburgerMenu style={{ height: "30px", width: "30px" }} />
+        <main className={styles.mainContainer}>
+            <nav className={`${styles.navigationBar} ${styles.resNavigation}`}>
+                <div className={styles.burger} >
+                    <GiHamburgerMenu
+                        onClick={hambergerMenu}
+                        style={{
+                            height: "30px",
+                            width: "30px",
+                            border: "2px solid black",
+                            borderRadius: "4px",
+                            fontSize: "1.3rem",
+                            margin: "4px"
+                        }} />
                 </div>
-                <ul className={`flexUl res-navbar ${isOpen ? "not-responsive" : ''}`}>
-                    <li className='lisItem'>
+                <ul className={`${styles.flexUl} ${styles.resNavbar} ${isOpen ? styles.no : styles.notResponsive}`}>
+                    <li className={styles.lisItem}>
                         <Link to="/">Home</Link>
                     </li>
-                    <li className='lisItem'>
+                    <li className={styles.lisItem}>
                         <Link to="/about">About</Link>
                     </li>
-                    <li className='lisItem'>
+                    <li className={styles.lisItem}>
                         <Link to="/service">Service</Link>
                     </li>
-                    <li className='lisItem'>
+                    <li className={styles.lisItem}>
                         <Link to="/product">Product</Link>
                     </li>
-                    <li className='lisItem'>
+                    <li className={styles.lisItem}>
                         <Link to="/team">Team</Link>
                     </li>
-                    <li className='lisItem'>
+                    <li className={styles.lisItem}>
                         <Link to="/blog">Blog</Link>
                     </li>
-                    <li className='lisItem'>
+                    <li className={styles.lisItem}>
                         <Link to="/career">Career</Link>
                     </li>
-                    <li className='lisItem'>
+                    <li className={styles.lisItem}>
                         <Link to="/director">Director</Link>
                     </li>
-                    <li className='lisItem'>
+                    <li className={styles.lisItem}>
                         <Link to="/contact">Contact</Link>
                     </li>
                 </ul>
